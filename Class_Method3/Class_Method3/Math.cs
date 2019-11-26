f992e96a44044d53d4605cfa7e7a6f2b1fecba52
ConnorSharpe <52796555+ConnorSharpe@users.noreply.github.com>
2019-11-26 15:51:30 -0800

    Added Class_method3

---
+++ Class_Method3/Class_Method3/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method3
+{
+    class Math
+    {
+        public int Math1(int num1)
+        {
+            int result = num1 * 2;
+            Console.WriteLine(result);
+            return result;
+        }
+        public int Math1(decimal num1)
+        {
+            decimal result1 = num1 *3;
+            Console.WriteLine(result1);
+            return Convert.ToInt32(result1);
+        }
+        public int Math1(string num1)
+        {
+            int convert = Convert.ToInt32(num1);
+            int result2 = convert * 4;
+            Console.WriteLine(result2);
+            return result2;
+        }
+    }
+}
---
+++ Class_Method3/Class_Method3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.Math1(12);
+
+            Math math2 = new Math();
+            math2.Math1(1.8m);
+
+            Math math3 = new Math();
+            math3.Math1("5");
+
+            Console.ReadLine();
+        }
+    }
+}
