e99d8b08445b7fba8887f702b52d77f6c63e11cc
ConnorSharpe <52796555+ConnorSharpe@users.noreply.github.com>
2019-11-26 14:55:00 -0800

    Add Class Method2

---
+++ Class_Methods2/Class_Methods2/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods2
+{
+    class Math
+    {
+        public void Math1(int num1, int num2)
+        {
+            int result = num1 / 2;
+            Console.WriteLine(num2);
+        }
+
+    }
+}
---
+++ Class_Methods2/Class_Methods2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            math.Math1(30, 15);
+            math.Math1(num1: 45, num2: 30);
+            Console.ReadLine();
+        }
+    }
+}
