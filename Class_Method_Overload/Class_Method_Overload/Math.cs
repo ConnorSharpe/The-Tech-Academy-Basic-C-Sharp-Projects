52fc733af0eed33d72de1025b4affe24ad8b21a6
ConnorSharpe <52796555+ConnorSharpe@users.noreply.github.com>
2019-11-26 17:01:03 -0800

    add Overload class method

---
+++ Class_Method_Overload/Class_Method_Overload/Math.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Overload
+{
+    class Math
+    {
+        public void Math1(int num1)
+        {
+            int num2 = num1 / 2;
+            Console.WriteLine(num2);
+        }
+
+        public void Math1(int num1, out int num2)
+        {
+            num2 = num1 / 2;
+        }
+
+    }
+}
---
+++ Class_Method_Overload/Class_Method_Overload/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Overload
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a whole number: ");
+            math.Math1(Convert.ToInt32(Console.ReadLine()));
+            Console.ReadLine();
+
+        }
+    }
+}
