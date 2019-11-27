b2d746313c794acd26e3ce5cdf5fe682338cfc08
ConnorSharpe <52796555+ConnorSharpe@users.noreply.github.com>
2019-11-26 16:11:50 -0800

    Add Class_methods3

---
+++ Class_methods4/Class_methods4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_methods4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please give me your favorite whole number: ");
+            int favNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please give me your second Favorite number. If you do not have one: just press enter.");
+            string favNum2 = Console.ReadLine();
+            if (favNum2 == "")
+            {
+                math.Math1(favNum);
+            }
+            else
+            {
+                int FavNum2 = Convert.ToInt32(favNum2);
+                math.Math1(favNum, FavNum2);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
