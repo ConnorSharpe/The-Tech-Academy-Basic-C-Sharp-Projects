937cc7da0b4502ba10bcbfd2e2f69e82e96667f4
ConnorSharpe <52796555+ConnorSharpe@users.noreply.github.com>
2019-12-09 18:56:26 -0800

    Added File IO Drill

---
+++ File_I_O/File_I_O/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_I_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your favorite number for the log?");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            File.WriteAllText(@"C:\Users\conno\Logs\logDrill.txt", answer.ToString());
+            Console.WriteLine(File.ReadAllText(@"C:\Users\conno\Logs\logDrill.txt"));
+            Console.Read();
+        }
+    }
+}
